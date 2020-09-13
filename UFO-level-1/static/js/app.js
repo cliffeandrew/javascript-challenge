f08acc1cafa616b395842947d2935f2086dc268d
Andrew Cliffe <acliffe45@gmail.com>
2020-09-12 23:36:41 -0700

    updating app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,24 +1,62 @@
 // from data.js
+
+//--Refernce to Table Body
 //var tableData = data;
 var tableData = d3.select("tbody");
+//var tableData2 = data;
 
 // YOUR CODE HERE!
+//--Initial Console Log
 console.log(data);
 
+//--Select Button
 var filter_btn = d3.select("#filter-btn");
-var datetime = d3.select("#datetime");
+//var datetime = d3.select("#datetime");
+//var filters = d3.select("#filters");
 
+//--Create Event Handlers
 filter_btn.on("click", runEnter);
-datetime.on("click", runEnter);
+//datetime.on("click", runEnter);
+//filters.on("click", runEnter);
+//filters.on("enter", runEnter);
 
+//--Complete Event Handler Function for the Form
 function runEnter() {
+
+    //--Prevent Refreshing
     d3.event.preventDefault();
+    
+    //--Select input element and get raw HTML node
     var inputElement = d3.select("#datetime");
+    //var inputElement = d3.select("#ufo-table");
+
+    //--Get value property of input element
     var inputValue = inputElement.property("value");
+
+    //--Print to Console
     console.log(inputValue);
-    console.log(people);
-    var filteredData = ufo.filter(ufo => ufo.datetime === inputValue);
+    console.log(tableData);
+
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    //var filteredData = tableData2.filter(ufo => ufo.datetime === inputValue);
+    
     console.log(filteredData);
+    
+    //--Create Array with Filtered Date
+    var selectdate = filteredData.map(ufo => ufo.datetime);
+
+    //--Remove Children from List
+    list.html("");
+
+    //--Append Rows to the List
+    list.append("li").text("datetime: {datetime}");
+    list.append("li").text("city: {city}");
+    list.append("li").text("state: {state}");
+    list.append("li").text("country: {country}");
+    list.append("li").text("shape: {shape}");
+    list.append("li").text("durationMinutes: {durationMinutes}");    
+    list.append("li").text("comments: {comments}");
 }
 
 
