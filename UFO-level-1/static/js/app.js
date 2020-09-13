a98e01eb4ac7671f9053098cff469e7d20de5329
Andrew Cliffe <acliffe45@gmail.com>
2020-09-12 22:03:08 -0700

    Data loads in html from app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,16 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
+var tableData = d3.select("tbody");
 
 // YOUR CODE HERE!
 console.log(data);
+
+
+data.forEach((ufosighting) => {
+    var row = tableData.append("tr");
+    Object.entries(ufosighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
