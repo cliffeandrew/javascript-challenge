472da904b77ba97cc6a26fbdc6583defa003f094
Andrew Cliffe <acliffe45@gmail.com>
2020-09-12 22:31:17 -0700

    filter_btn and datetime runEnter events

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,6 +5,23 @@ var tableData = d3.select("tbody");
 // YOUR CODE HERE!
 console.log(data);
 
+var filter_btn = d3.select("#filter-btn");
+var datetime = d3.select("#datetime");
+
+filter_btn.on("click", runEnter);
+datetime.on("click", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(people);
+    var filteredData = ufo.filter(ufo => ufo.datetime === inputValue);
+    console.log(filteredData);
+}
+
+
 
 data.forEach((ufosighting) => {
     var row = tableData.append("tr");
